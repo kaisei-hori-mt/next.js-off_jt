208720af3f517ffc4a3e426f6999d16700c67324
MT 堀 開晴 <kaisei.hori@metateam.co.jp>
2025-05-21 16:07:46 +0900

    演習課題2 年齢項目の実装

--- src/app/Form/page.tsx
+++ src/app/Form/page.tsx
@@ -7,6 +7,7 @@ import type { SubmitHandler } from "react-hook-form";
 type FormValues = {
 	name: string;
 	email: string;
+	age: number;
 };
 
 export default function Form() {
@@ -43,7 +44,7 @@ export default function Form() {
 							required: "名前は必須です",
 						})}
 					/>
-					{errors.name && <span>{errors.name.message}</span>}
+					{errors.name && <p>{errors.name.message}</p>}
 				</div>
 				<div>
 					<label htmlFor="email">メールアドレス</label>
@@ -53,9 +54,32 @@ export default function Form() {
 						autoComplete="email"
 						{...register("email", {
 							required: "メールアドレスは必須です",
+							pattern: {
+								value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/,
+								message: "メールアドレスの形式が正しくありません",
+							},
 						})}
 					/>
-					{errors.email && <span>{errors.email.message}</span>}
+					{errors.email && <p>{errors.email.message}</p>}
+				</div>
+				{/* 課題2 */}
+				<div>
+					<label htmlFor="age">年齢</label>
+					<input
+						type="number"
+						id="age"
+						autoComplete="age"
+						{...register("age", {
+							required: "年齢は必須です",
+							pattern: {
+								value: /^[0-9]+$/,
+								message: "年齢は数値で入力してください",
+							},
+							min: { value: 18, message: "年齢は18歳以上で入力してください" },
+							max: { value: 100, message: "年齢は100歳以下で入力してください" },
+						})}
+					/>
+					{errors.age && <p>{errors.age.message}</p>}
 				</div>
 				<button type="submit">送信</button>
 			</form>
