d990bf73e76c4dd0c24a5234adcd69b323864816
MT 堀 開晴 <kaisei.hori@metateam.co.jp>
2025-05-26 12:28:40 +0900

    API通信演習課題 メモ一覧ページでAPI取得データの表示

---
+++ src/app/api/memos/route.ts
@@ -0,0 +1,22 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const dummyData = [
+  { id: 1, title: "メモ1" },
+  { id: 2, title: "メモ2" },
+];
+
+export async function GET() {
+  return NextResponse.json(dummyData);
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const newMemo = {
+    id: dummyData.length + 1,
+    title: body.title ?? "(無題)",
+  };
+  dummyData.push(newMemo);
+  return NextResponse.json({ success: true, message: "登録完了" });
+}
