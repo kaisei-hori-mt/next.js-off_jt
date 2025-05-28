346bc8373aca4c417499cc9e27094634581a8fe7
MT 堀 開晴 <kaisei.hori@metateam.co.jp>
2025-05-28 16:12:10 +0900

    修正：タグの変更・修正、"use client"の削除

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,18 +5,16 @@ export const Header = () => {
   return (
     <header className="p-4 bg-gray-100 flex justify-between items-center">
       <h1 className="text-lg font-bold">My App</h1>
-      <div>
-        {user ? (
-          <>
-            <span className="mr-2">{user.name} さん</span>
-            <button onClick={logout} type="button" className="text-blue-500">
-              ログアウト
-            </button>
-          </>
-        ) : (
-          <span>ゲスト</span>
-        )}
-      </div>
+      {user ? (
+        <div>
+          <p className="mr-2">{user.name} さん</p>
+          <button onClick={logout} type="button" className="text-blue-500">
+            ログアウト
+          </button>
+        </div>
+      ) : (
+        <p>ゲスト</p>
+      )}
     </header>
   );
 };
