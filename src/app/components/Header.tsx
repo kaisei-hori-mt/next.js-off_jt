8b4656a08bb63d38fae606c1932c81d5f8ba3878
MT 堀 開晴 <kaisei.hori@metateam.co.jp>
2025-05-30 11:23:44 +0900

    修正：divタグ -> 空タグ

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,12 +6,12 @@ export const Header = () => {
     <header className="p-4 bg-gray-100 flex justify-between items-center">
       <h1 className="text-lg font-bold">My App</h1>
       {user ? (
-        <div>
+        <>
           <p className="mr-2">{user.name} さん</p>
           <button onClick={logout} type="button" className="text-blue-500">
             ログアウト
           </button>
-        </div>
+        </>
       ) : (
         <p>ゲスト</p>
       )}
